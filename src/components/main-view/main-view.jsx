18557d898ff3b1b9caaec0c667bb62167f8883c3
Techmo27 <e.vins@yahoo.com>
2022-01-17 10:56:37 +0100

    Created Hooks for Registration and Login View

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -35,6 +36,14 @@ export class MainView extends React.Component {
     });
   }
 
+  /* When a user successfully registers, this function is triggered and updates the "register" property */
+
+  onRegistration(register) {
+    this.setState({
+      register
+    });
+  }
+
   /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
 
   onLoggedIn(user) {
@@ -44,7 +53,9 @@ export class MainView extends React.Component {
   }
 
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
+
+    if (!register) return <RegistrationView onRegistration={register => this.onRegistration(register)} />;
 
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
     if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
