ddb72c5324b6722fec765b80a667ef316a618a6e
Techmo27 <e.vins@yahoo.com>
2022-01-18 11:13:27 +0100

    Added Bootstrap Styling to Registration Form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row, Navbar, Nav } from 'react-bootstrap';
 
 import './registration-view.scss';
 
@@ -19,25 +20,55 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label>
-        Birthday:
-        <input type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-      </label>
-      <button type="submit" onClick={handleSubmit}>Register</button>
-    </form>
+    <>
+      <Navbar expand="sm" bg="dark" variant="dark" className="loginNavbar">
+        <Container fluid>
+          <Navbar.Brand href="#myflix">My Flix</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#movies">Movies</Nav.Link>
+            <Nav.Link href="#favorite-movies">My Favorites</Nav.Link>
+            <Nav.Link href="#profile">Account</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <Container >
+        <Row>
+          <Col xs={1} sm={1} md={2} lg={3} ></Col>
+          <Col>
+            <Card style={{ marginTop: 100, marginBottom: 50, maxWidth: '30rem' }}>
+              <Card.Body>
+                <Card.Title style={{ textAlign: 'center', fontSize: '2rem' }}>Create a New Account</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control type="text" placeholder="Enter a username" value={username} onChange={e => setUsername(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control type="password" placeholder="Enter a password" value={password} onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control type="email" placeholder="Enter your email address" value={email} onChange={e => setEmail(e.target.value)} />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Birthday:</Form.Label>
+                    <Form.Control type="date" placeholder="Enter your birthday(optional)" value={birthday} onChange={e => setBirthday(e.target.value)} />
+                  </Form.Group>
+
+                  <Button style={{ marginTop: 10 }} variant="info" type="submit" onClick={handleSubmit}>Register</Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={1} sm={1} md={2} lg={3} ></Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
